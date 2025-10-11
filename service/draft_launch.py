6f15db7760a5bbcd804e0c785527f260a32e789b
yokamoto5742-h <yokamoto5742@gmail.com>
2025-10-11 15:25:55 +0900

    ✨ feat(project_structure): 更新日時と不要なimportを削除しました 🔧 fix(draft_launch): Chromeのパスの存在確認を追加しました

--- service/draft_launch.py
+++ service/draft_launch.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 
 from utils.config_manager import load_config
@@ -6,6 +7,10 @@ from utils.config_manager import load_config
 def launch_draft_page():
     config = load_config()
     chrome_path = config.get('Chrome', 'chrome_path')
+
+    if not os.path.exists(chrome_path):
+        chrome_path = config.get('Chrome', 'chrome_x86_path')
+
     draft_url = config.get('URL', 'draft_url').strip('"')
     subprocess.Popen([chrome_path, draft_url])
 
